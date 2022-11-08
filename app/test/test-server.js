bd8ec32d8768748713d98da865408170db916261
Benedek4000 <kovacsbenedek4000@gmail.com>
2022-11-08 14:23:16 +0000

    changed test-server.js

--- app/test/test-server.js
+++ app/test/test-server.js
@@ -19,7 +19,7 @@ describe('Homepage', function() {
     chai.request(server)
       .get('/')
       .end(function(err, res){
-        res.text.should.contain('Spartatest')
+        res.text.should.contain('Sparta')
         done();
       });
   });
